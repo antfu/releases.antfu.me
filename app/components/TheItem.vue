961d27970224b9d02d43b4907cf6506635ed79de
Anthony Fu <github@antfu.me>
2024-08-20 17:56:02 +0200

    feat: update design

--- app/components/TheItem.vue
+++ app/components/TheItem.vue
@@ -39,7 +39,7 @@ const subImage = computed(() => {
 <template>
   <div
     flex="~ gap-4 items-center"
-    lt-sm="gap-4"
+    lt-sm="gap-6"
     :class="timeDiffHours > 14 ? 'mt-8' : ''"
   >
     <a
@@ -49,6 +49,7 @@ const subImage = computed(() => {
       <img
         :src="logoOverrides[item.repo] || `https://github.com/${item.repo.split('/')[0]}.png`"
         h-12 w-12 :alt="item.repo"
+        lt-sm="h-14 w-14"
         border="~ gray/5" bg-gray:5
         :class="item.isOrg === false && !logoOverrides[props.item.repo] ? 'rounded-full' : 'rounded'"
       >
@@ -66,26 +67,25 @@ const subImage = computed(() => {
 
     </a>
 
-    <div flex="~ row gap-3 auto" lt-sm="flex-col">
-      <div flex-auto flex="~ col gap-1">
+    <div flex="~ row gap-2 auto items-center" lt-sm="flex-col items-start">
+      <div flex-auto flex="~ col">
         <div flex="~ gap-2 items-center">
           <a
-            flex="~ gap-1 wrap items-center" text-xl
+            flex="~ col wrap"
             :href="`https://github.com/${item.repo}`" target="_blank"
           >
-            <div op75>{{ item.repo.split('/')[0] }}</div>
-            <div op50>/</div>
-            <div>{{ item.repo.split('/')[1] }}</div>
+            <div op50>{{ item.repo.split('/')[0] }}<span op75>/</span></div>
+            <div mt--0.5 text-1.4rem>{{ item.repo.split('/')[1] }}</div>
           </a>
         </div>
-        <a :href="item.commit" target="_blank" flex="~ gap-1 items-center" ml--1 op50>
+        <a :href="item.commit" target="_blank" flex="~ gap-0.5 items-center" ml--1 op50 lt-sm="hidden">
           <div i-ph-git-commit-duotone rotate-90 />
           {{ item.title }}
         </a>
       </div>
       <div flex="~ col items-end" lt-sm="flex-row gap-2" text-end>
         <a
-          font-mono
+          text-1.1rem font-mono
           :href="`https://github.com/${item.repo}/releases/tag/v${item.version}`" target="_blank"
         >
           v{{ item.version }}
